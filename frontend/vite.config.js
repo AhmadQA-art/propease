821a02a304b86d684a06f94fadfa252e3399d571
AhmadQA-art <ahmadmesbahqa@gmail.com>
2025-04-19 22:32:08 +0200

    fixing ui issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,6 +13,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  css: {
+    postcss: './postcss.config.cjs',
+  },
   build: {
     outDir: 'build',
   },
